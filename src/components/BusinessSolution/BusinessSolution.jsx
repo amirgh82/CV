61be9fa2f672fb26c522d9cdaa263018c0f53a02
amirgh82 <amirghhastam@gmail.com>
2023-05-05 00:28:16 +0330

    Add Business solution images

--- src/components/BusinessSolution/BusinessSolution.jsx
+++ src/components/BusinessSolution/BusinessSolution.jsx
@@ -5,7 +5,9 @@ export default function BusinessSolution() {
     return (
         <div className='business-solution'>
             <div className="business-solution-picture">
-
+                <img src="/images/business-solution1.jpg" className="business-solution-img image-business" />
+                <img src="/images/business-solution2.jpg" className="business-solution-img image-business" />
+                <img src="/images/business-solution3.jpg" className="business-solution-img image-business" />
             </div>
             <div className="business-solution-detail">
 
