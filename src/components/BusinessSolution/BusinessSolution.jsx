1fa3ea4480122a6c6405262570edae0480ee9147
amirgh82 <amirghhastam@gmail.com>
2023-05-04 21:06:30 +0330

    Add Business Solution component

---
+++ src/components/BusinessSolution/BusinessSolution.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './BusinessSolution.css'
+
+export default function BusinessSolution() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,10 @@ import Header from '../../components/Header/Header'
 import Features from '../../components/Features/Features'
 import { FcBarChart, FcSelfServiceKiosk, FcVideoProjector } from 'react-icons/fc'
 import { MdAccessibilityNew } from 'react-icons/md'
-
 import './Home.css'
+import BusinessSolution from '../../components/BusinessSolution/BusinessSolution'
+
+
 export default function Home() {
 
     return (
@@ -16,6 +18,7 @@ export default function Home() {
                 <Features icon={<FcVideoProjector />} title='آی کیو و تست' detail='ساخته شده به زبان شبه لاتین که.' ></Features>
                 <Features icon={<MdAccessibilityNew />} title='تیم اختصاصی' detail='ساخته شده به زبان شبه لاتین که.' ></Features>
             </div>
+            <BusinessSolution />
         </div>
     )
 }
