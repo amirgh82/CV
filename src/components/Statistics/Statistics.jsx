5b468493bf429fe90778827e657872558e5b7522
amirgh82 <amirghhastam@gmail.com>
2023-05-10 21:15:46 +0330

    Completion of the section statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import './Statistics.css'
 import { RiUser6Line } from 'react-icons/ri'
 import { BsCalendarWeek } from 'react-icons/bs'
@@ -7,36 +7,29 @@ import { BiBrain } from 'react-icons/bi'
 export default function Statistics() {
 
 
-    const statisticsRef = useRef()
-    const numberRef = useRef()
-
-    const windowScrollHandler = () =>{
-
-    }
-
-    
-
 
     return (
-        <div className='statistics' ref={statisticsRef}>
+        <div className='statistics' >
             <div className="statistics-customer statistic">
                 <RiUser6Line />
-                <span className="number" data-count='9759' ref={numberRef} >1</span>
+                <span className="number counter" >0</span>
+
+
                 <p className="statistics-detail">مشتریان خوشحال</p>
             </div>
             <div className="statistics-history statistic">
                 <BsCalendarWeek />
-                <span className="number" data-count='15' ref={numberRef} >1</span>
+                <span className="number" data-count='15'  >1</span>
                 <p className="statistics-detail">سال تجربه</p>
             </div>
             <div className="statistics-customer statistic">
                 <TiTickOutline />
-                <span className="number" data-count='151' ref={numberRef} >1</span>
+                <span className="number" data-count='151'  >1</span>
                 <p className="statistics-detail">پروژه های تکمیلی</p>
             </div>
             <div className="statistics-customer statistic">
                 <BiBrain />
-                <span className="number" data-count='98' ref={numberRef} >1</span>
+                <span className="number" data-count='98'  >1</span>
                 <p className="statistics-detail"> مهندسین خبره</p>
             </div>
         </div>
